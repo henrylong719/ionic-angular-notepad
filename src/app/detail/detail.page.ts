42c9eb686c25182ba877c02d81cea1c8a001a8c7
henrylong <henrylong719@gmail.com>
2021-04-21 19:25:33 +0930

    finish up notepad

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { Note } from '../interfaces/note';
+import { NotesService } from '../services/notes.service';
 
 @Component({
   selector: 'app-detail',
@@ -6,10 +11,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.page.scss'],
 })
 export class DetailPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public note: Note;
+  paramsSub: Subscription;
+  constructor(
+    private route: ActivatedRoute,
+    private noteService: NotesService,
+    private navCtrl: NavController
+  ) {
+    this.note = {
+      id: '',
+      title: '',
+      content: '',
+    };
   }
 
+  ngOnInit() {
+    let noteId;
+    this.paramsSub = this.route.params.subscribe((params: Params) => {
+      noteId = params.id;
+    });
+
+    if (this.noteService.loaded) {
+      this.note = this.noteService.getNote(noteId);
+    } else {
+      this.noteService.load().then(() => {
+        this.note = this.noteService.getNote(noteId);
+      });
+    }
+  }
+
+  noteChanged() {
+    this.noteService.save();
+  }
+
+  deleteNote() {
+    this.noteService.deleteNote(this.note);
+    this.navCtrl.navigateBack('/notes');
+  }
 }
